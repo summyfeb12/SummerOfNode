12d6a2853652a718412b6d41771a1b80566c8726
Sumanth Reddy <summyfeb12@gmail.com>
2016-06-20 15:55:47 -0500

    sample chat with socket

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection',function(socket){
+  console.log('A user Connected');
+  socket.on('disconnect',function(){
+    console.log('User Disconnected');
+  });
+  socket.on('chat message',function(msg){
+    console.log('message: '+ msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
