4cda0994caf2ee130abd91ec9eb8bbc4b9abc416
Sumanth Reddy <summyfeb12@gmail.com>
2016-11-21 10:20:33 -0600

    test for linux

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var xml = require('xml');
 var sample=[ { toys: [ { toy: 'Transformers' } , { toy: 'GI Joe' }, { toy: 'He-man' } ] } ];
 app.get('/', function(req, res){
   res.set('Content-Type', 'text/xml');
-  console.log(sample);
+  //console.log(sample);
   console.log(xml(sample));
   res.send(xml(sample));
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
